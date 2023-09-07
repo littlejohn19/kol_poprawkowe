2a57a85140b5935c19b5c3ff161afddb0556fa4a
Darek <darekpiwko@outlook.com>
2023-09-07 09:11:05 +0200

    kol

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+
+function App() {
+
+  return (
+      <div className="container-fluid">
+        {/*nav bar*/}
+        <div className="container">
+          {/*outlet*/}
+        </div>
+      </div>
+  );
+}
+
+export default App;
